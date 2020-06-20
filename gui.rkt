f649727300e3fa5c7a902bbe0a287681b0d5cc90
Lumanter <luismariano10del4@gmail.com>
2020-06-20 10:57:08 -0600

    Created linear functions to adjust character size and position based on grid dimensions

--- gui.rkt
+++ gui.rkt
@@ -3,8 +3,8 @@
 (require "logic-utilities.rkt")
 
 ;; Global Variables
-(define rows 3)
-(define columns 3)
+(define global-rows 10)
+(define global-columns 10)
 
 
 (define user-move #\X)
@@ -12,36 +12,14 @@
 (define new-game #t)
 (define current-move user-move)
 
-(define game-grid (get-matrix 3 3 "_"))
+(define game-grid (get-matrix global-rows global-columns "_"))
 
 ;; Window to play the game
 (define game-frame (new frame%
                    [label "Tic Tac Racket"]
-                   [width 400]
-                   [height 400]))
+                   [width 600]
+                   [height 600]))
 
-;; Panes
-;; Main Pane
-(define main-pane (new vertical-pane%
-                      [parent game-frame]
-                      [vert-margin 5]
-                      [horiz-margin 5]
-                      [spacing 5]))
-;; Child Panes
-(define upper-pane (new horizontal-pane%
-                        [parent main-pane]
-                        [spacing 5]))
-
-(define middle-pane
-  (new horizontal-pane%
-       [parent main-pane]
-       [spacing 5]))
-(define lower-pane
-  (new horizontal-pane%
-       [parent main-pane]
-       [spacing 5]))
-
-;; Draw Canvas
 ;; Canvas-box class
 (define canvas-box%
   (class canvas%
@@ -73,8 +51,8 @@
       (let ((dc (get-dc)))        
         (let-values (((x y) (send this get-size)))
           (send dc set-text-foreground color)
-          (send dc set-font (make-object font% 50 'default))
-          (send dc draw-text (string character) (/ (- x 50) 2) (/ (- y 75) 2)))))
+          (send dc set-font (make-object font% (+ 60 (* -2 (+ global-rows global-columns))) 'default))
+          (send dc draw-text (string character) (/ (- x (+ 60 (* -2 (+ global-rows global-columns)))) 2) (/ (- y (+ 93 (* -3 (+ global-rows global-columns)))) 2)))))
    
     (define/public (set-color c)
       (set! color c))
@@ -84,60 +62,40 @@
    
     (super-new)))
 
-;; Instance canvases
-(define canvases
-  (let ((canvas-one (new canvas-box%
-                         [position 0]
-                         [row 0]
-                         [column 0]
-                         [parent upper-pane]))
-        (canvas-two (new canvas-box%
-                         [position 1]
-                         [row 0]
-                         [column 1]
-                         [parent upper-pane]))
-        (canvas-three (new canvas-box%
-                           [position 2]
-                           [row 0]
-                           [column 2]
-                           [parent upper-pane]))
-        (canvas-four (new canvas-box%
-                          [position 3]
-                          [row 1]
-                          [column 0]
-                          [parent middle-pane]))
-        (canvas-five (new canvas-box%
-                          [position 4]
-                          [row 1]
-                          [column 1]
-                          [parent middle-pane]))
-        (canvas-six (new canvas-box%
-                         [position 5]
-                         [row 1]
-                         [column 2]
-                         [parent middle-pane]))
-        (canvas-seven (new canvas-box%
-                           [position 6]
-                           [row 2]
-                           [column 0]
-                           [parent lower-pane]))
-        (canvas-eight (new canvas-box%
-                           [position 7]
-                           [row 2]
-                           [column 1]
-                           [parent lower-pane]))
-        (canvas-nine (new canvas-box%
-                          [position 8]
-                          [row 2]
-                          [column 2]
-                          [parent lower-pane])))  
-    (list canvas-one canvas-two canvas-three
-          canvas-four canvas-five canvas-six
-          canvas-seven canvas-eight canvas-nine)))
+;; Panes
+;; Main Pane
+(define main-pane (new vertical-pane%
+                      [parent game-frame]
+                      [vert-margin 5]
+                      [horiz-margin 5]
+                      [spacing 5]))
 
-; method get-parent-panel
+;; Creates and links the game grid panes to hold the canvases
+(define (get-panes row)
+  (cond
+    ((>= row global-rows) '())
+    (else (cons (new horizontal-pane% [parent main-pane] [spacing 5])
+                (get-panes (+ row 1))))))
+
+;; Creates and links the game grid canvases to the panes
+(define (get-panes-canvases panes row)
+  (cond
+    ((null? panes) '())
+    (else (cons (get-pane-canvases (car panes) row 0) (get-panes-canvases (cdr panes) (+ row 1))))))
+
+;; Creates and links the canvases to a single pane
+(define (get-pane-canvases pane row column)
+  (cond
+    ((>= column global-columns) '())
+    (else (cons (new canvas-box%
+                   [row row]
+                   [column column]
+                   [parent pane])
+                (get-pane-canvases pane row (+ column 1))))))
 
 
+(define panes (get-panes 0))
+(define canvases (get-panes-canvases panes 0))
 
 ;; Window to input the game grid's rows and columns
 (define input-frame (new frame%
@@ -171,8 +129,8 @@
                    [min-height 50]
                    [vert-margin 15]
                    [callback (λ (b e) 
-                     (set! rows (string->number (send rows-input get-string-selection)))
-                     (set! columns (string->number (send columns-input get-string-selection)))
+                     (set! global-rows (string->number (send rows-input get-string-selection)))
+                     (set! global-columns (string->number (send columns-input get-string-selection)))
                      ; set game-grid
                      ; set panes
                      ; set canvases
